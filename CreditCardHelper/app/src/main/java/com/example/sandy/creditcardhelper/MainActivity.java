42f3b209d4579615ffac3f8bce9d1283d2979d50
Sandeep Hegde <sandeep.hegde123@gmail.com>
2017-05-02 20:36:06 +0530

    Implement the UI for a credit card application
    Implementing the UI for a Credit Card application that will help people
    with their credit card debt

---
+++ CreditCardHelper/app/src/main/java/com/example/sandy/creditcardhelper/MainActivity.java
@@ -0,0 +1,19 @@
+//Android Developer Training Session 2 - Assignment 2.2
+
+/*PROBLEM STATEMENT- You need to implement the UI for a Credit Card application
+ that will help people with their credit card debt */
+
+
+package com.example.sandy.creditcardhelper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
